25fbb9fd687d5d9239b4ecfebf1e9096755df3d8
jaciteixeira <rm99627@fiap.com.br>
2023-09-13 08:51:06 -0300

    useState/useNavigate, destructuring

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { ListaProdutos } from "../components/ListaProdutos";
 import { useState } from "react";
 
@@ -6,19 +6,15 @@ export default function EditarProdutos() {
 
   document.title = "Editar Produtos";
 
+  const navigate = useNavigate();
+
   //Recuperando o id do produto com o HOOK useParam();
   const {id} = useParams();
 
     //Utilizando o filter na Lista de Produtos para recuperar um produto através do id como parâmetro.
-    // const prodRecuperadoPorId = ListaProdutos.filter( (produto) => {
-    //     if(produto.id == id){
-    //       return produto;
-    //     }
-    // });
+    const prodRecuperadoPorId = ListaProdutos.filter( produto => produto.id == id);
 
-    const prodRecuperadoPorId = ListaProdutos.filter(produto => produto.id == id);
-
-    const[produto,setProduto] = useState({
+    const [produto,setProduto] = useState({
       id: prodRecuperadoPorId[0].id,
       nome: prodRecuperadoPorId[0].nome,
       preco: prodRecuperadoPorId[0].preco,
@@ -26,34 +22,74 @@ export default function EditarProdutos() {
       img: prodRecuperadoPorId[0].img
     });
 
-    const handlerEditarProduto = (event) => {
-      event.preventDefault();
+    const handleChange = (event) => {
+        // console.log(event.target);
+
+        //Destructuring
+        const {name,value} = event.target;
+        //Inserir os dado no objeto produto através do setProduto({...});
+        setProduto({...produto,[name]:value});
+
     }
+
+    const handleSubmit = (event)=>{
+      
+      event.preventDefault();
+
+      //Índice que será utilizado para a sobreposição do produtos na lista.
+      let indice;
+
+      //Localização do índice na lista.
+      ListaProdutos.forEach((item,index)=>{
+        if(item.id == produto.id){
+          indice = index;
+        }       
+      });
+
+      //Utilizando o método splice para alterar o produto no índice especificado.
+      ListaProdutos.splice(indice,1,produto);
+      alert("Produto alterado!");
+
+      navigate("/produtos")
+
+    }
+
 
   return (
     <>
         <div>
+
+
             <h1>EDITAR-PRODUTOS</h1>
-            <form action="#" method="get">
+            <form onSubmit={handleSubmit}>
               <fieldset>
                 <legend>EDITAR PRODUTO</legend>
                 <div>
-                <label htmlFor="idProduto">Nome Produto:</label>
-                  <input type="text" name="nome" id="idProduto" defaultValue={produto.nome} onChange={handlerEditarProduto}/>
+                  <label htmlFor="idProduto">Nome Produto:</label>
+                  <input type="text" name="nome" id="idProduto" value={produto.nome} onChange={handleChange} />
                 </div>
                 <div>
                   <label htmlFor="idPreco">Preço Produto:</label>
-                  <input type="text" name="nome" id="idProduto" defaultValue={produto.nome} onChange={handlerEditarProduto} />
+                  <input type="text" name="preco" id="idPreco" value={produto.preco} onChange={handleChange} />
                 </div>
-                
                 <div>
-                  <button>EDITAR</button>
+                  <label htmlFor="idDesc">Descrição Produto:</label>
+                  <input type="text" name="desc" id="idDesc" value={produto.desc} onChange={handleChange} />
+                </div>
+
+                <div>
+                  <button >EDITAR</button>
                 </div>
               </fieldset>
             </form>
-            <p>NOME: - {prodRecuperadoPorId[0].nome}</p>
-            <p>PREÇO: - {prodRecuperadoPorId[0].preco}</p>
         </div>
+
+        <div>
+          <p>Nome : {produto.nome}</p>
+          <p>Preço : {produto.preco}</p>
+          <p>Desc : {produto.desc}</p>
+        </div>
+
     </>
   )
 }
