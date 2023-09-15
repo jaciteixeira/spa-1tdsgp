7affb1988af5749f5adf04c54006805601053908
jaciteixeira <rm99627@fiap.com.br>
2023-09-15 11:13:44 -0300

    Exemplos de useEffects 3x (3 exemplos)

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -9,9 +9,23 @@ export default function Produtos() {
 
     const [counter, setCounter] = useState(0)
 
+    const [novaListaProdutos, setNovaListaProdutos] = useState([{}]);
+
+    const [counter2, setCounter2] = useState(0);
+
     useEffect(()=>{
         console.log("useEffect que renderiza sempre!")
     })
+
+    useEffect(()=>{
+        setNovaListaProdutos(ListaProdutos);
+        console.log("useEffect que renderiza apenas uma vez!")
+   
+    },[ ])
+
+    useEffect(() => {
+        console.log("useEffect que renderiza apenas se o objeto/elemento/constante que está sendo monitorado no array de dependências sofrer atualização.");
+    },[counter2]);
 
     return (
     <>
@@ -20,6 +34,9 @@ export default function Produtos() {
 
             <div>
                 <button onClick={()=> setCounter(counter + 1)}>COUNTER - {counter}</button>
+            </div>
+            <div>
+                <button onClick={()=> setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
             </div>
 
             <table className={classes.tabelaProd}>
@@ -35,7 +52,7 @@ export default function Produtos() {
                     </tr>
                 </thead>
                 <tbody>
-                    {ListaProdutos.map( (produto, indice)=>(
+                    {novaListaProdutos.map( (produto, indice)=>(
                             <tr className={classes.tabelaLinha} key={indice}>
                                 <td className={classes.tabelaDados}>{produto.id}</td>
                                 <td className={classes.tabelaDados}>{produto.nome}</td>
