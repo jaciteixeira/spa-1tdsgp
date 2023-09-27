ca9ce9d63bf1c377f4bbe815de3fa557c080e9ab
jaciteixeira <rm99627@fiap.com.br>
2023-09-27 09:44:22 -0300

    aula 27/09 Inserir Produto com POST na api db.json

---
+++ src/components/ModalExemplo/ModalExemplo.jsx
@@ -0,0 +1,124 @@
+import { useState } from "react";
+import style from "./ModalExemplo.module.css";
+
+export default function ModalExemplo(props) {
+  document.title = "CADASTRO";
+
+  let novoId;
+
+  fetch("http://localhost:5000/produtos", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      novoId = data[data.length - 1].id + 1;
+    })
+    .catch((error) => console.log(error));
+
+    const [produto, setProduto] = useState({
+      id: novoId,
+      nome:"",
+      preco:"",
+      desc:""
+    });
+
+  const handleChange = (e) =>{
+    e.preventDefault();
+    
+    // const{name,value} = e.target;
+
+    // if(name == "nome"){
+    //   setProduto({"nome": value, "preco": "","desc": ""});
+    // }else if(name == "preco"){
+    //   setProduto({"nome": "", "preco": value,"desc": ""});
+    // }else if(name == "desc"){
+    //   setProduto({"nome": "", "preco": "","desc": value});
+    // }
+    
+    const{name,value} = e.target;
+    setProduto({...produto,[name]:value});
+  }
+
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    
+    fetch("http://localhost:5000/produtos",{
+      method:'POST',
+      headers:{
+        "Content-Type":"application/json",
+      },
+      body: JSON.stringify(produto)
+    })
+    .then((response)=> {
+        console.log("Status da requisição HTTP : " + response.status);
+        return response.json();
+      })
+    .then((data)=> console.log(data))
+    .catch(error => console.log(error));
+    props.setOpen(false)
+
+  }
+
+  if (props.open) {
+    return (
+      <div className={style.container}>
+        <h1>Cadastrar Produto</h1>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <fieldset>
+              <legend>Novo Produto</legend>
+              <div>
+                <label htmlFor="idProduto">Nome Produto:</label>
+                <input
+                  type="text"
+                  name="nome"
+                  id="idProduto"
+                  value={produto.nome}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="idPreco">Preço Produto:</label>
+                <input
+                  type="text"
+                  name="preco"
+                  id="idPreco"
+                  value={produto.preco}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="idDesc">Descrição Produto:</label>
+                <input
+                  type="text"
+                  name="desc"
+                  id="idDesc"
+                  value={produto.desc}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="idDesc">Imagem do Produto:</label>
+                <input
+                  type="text"
+                  name="img"
+                  id="idImg"
+                  value={produto.img}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div>
+                <button>CADASTRAR</button>
+              </div>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,43 +1,44 @@
 import { useEffect, useState } from "react";
 import { AiOutlineEdit as Editar, AiOutlineDelete as Excluir } from "react-icons/ai";
 import { Link } from "react-router-dom";
-import { ListaProdutos } from "../components/ListaProdutos";
+// import { ListaProdutos } from "../components/ListaProdutos";
 import classes from "./Produtos.module.css";
+import ModalExemplo from "../components/ModalExemplo/ModalExemplo";
 
 
 export default function Produtos() {
+    document.title = "Produtos";
 
-    const [counter, setCounter] = useState(0)
+    const [novaListaProdutos, setNovaListaProdutos] = useState([{}]); 
 
-    const [novaListaProdutos, setNovaListaProdutos] = useState([{}]);
-
-    const [counter2, setCounter2] = useState(0);
-
-    useEffect(()=>{
-        console.log("useEffect que renderiza sempre!")
-    })
-
-    useEffect(()=>{
-        setNovaListaProdutos(ListaProdutos);
-        console.log("useEffect que renderiza apenas uma vez!")
-   
-    },[ ])
+    const [open, setOpen] = useState(false);
 
     useEffect(() => {
-        console.log("useEffect que renderiza apenas se o objeto/elemento/constante que está sendo monitorado no array de dependências sofrer atualização.");
-    },[counter2]);
+        if (!open) {
+          //fetch = API do Javascript para realizar requisições/requests, ele utiliza como parâmetro uma URL ou URI.
+          //fetch(http://minhaApi.com.br/exemplos)
+          fetch("http://localhost:5000/produtos", {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              setNovaListaProdutos(data);
+            })
+            .catch((error) => console.log(error));
+        }
+      }, [open]);
+    
 
     return (
     <>
         <div>
             <h1>PRODUTOS</h1>
 
-            <div>
-                <button onClick={()=> setCounter(counter + 1)}>COUNTER - {counter}</button>
-            </div>
-            <div>
-                <button onClick={()=> setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
-            </div>
+            { open ? <ModalExemplo open={open} setOpen={setOpen}/> : "" }
+            <Link onClick={()=> setOpen(true)}>Adicionar Produto</Link>
 
             <table className={classes.tabelaProd}>
                 <thead className={classes.tabelaCabecalho}>
@@ -47,43 +48,44 @@ export default function Produtos() {
                         <th>DESCRIÇÃO</th>
                         <th>PREÇO</th>
                         <th>IMG</th>
-                        <th><Editar/> / <Excluir/></th>i was here since yesterday
-
+                        <th><Editar/> / <Excluir/></th>
                     </tr>
                 </thead>
                 <tbody className={classes.tabelaCorpo}>
                     {novaListaProdutos.map( (produto, indice)=>(
-                            <tr key={indice}>
-                                <td>{produto.id}</td>
-                                <td>{produto.nome}</td>
-                                <td>{produto.desc}</td>
-                                <td>{produto.preco}</td>
-                                <td>
-                                    <img className={classes.tblImg} 
-                                    src={`${produto.img}`} 
-                                    alt={`${produto.desc}`}/>
-                                </td>
-                                <td> 
-                                    {" "}
-                                    <Link to={`/editar/produto/${produto.id}`}>
-                                        <Editar/>
-                                    </Link> {" "}
-                                    / {" "}
-                                    <Link to={`/excluir/produto/${produto.id}`}>
-                                        <Excluir/>
-                                    </Link> {" "}
-                                </td>
-                            </tr>
+                        <tr key={indice}>
+                            <td>{produto.id}</td>
+                            <td>{produto.nome}</td>
+                            <td>{produto.desc}</td>
+                            <td>{produto.preco}</td>
+                            <td>
+                                <img className={classes.tblImg} 
+                                src={`${produto.img}`} 
+                                alt={`${produto.desc}`}/>
+                            </td>
+                            <td> 
+                                {" "}
+                                <Link to={`/editar/produto/${produto.id}`}>
+                                    <Editar/>
+                                </Link> {" "}
+                                / {" "}
+                                <Link to={`/excluir/produto/${produto.id}`}>
+                                    <Excluir/>
+                                </Link> {" "}
+                            </td>
+                        </tr>
                     ))}
                 </tbody>
                 <tfoot>
                     <tr>
-                        <td colSpan={6}>PRODUTOS</td>
+                        <td colSpan={6}>
+                            PRODUTOS INFORMÁTICOS - QTD = {novaListaProdutos.length}
+                        </td>
                     </tr>
                 </tfoot>
             </table>
 
         </div>
     </>
-  )
+  );
 }
